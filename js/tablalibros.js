ccfcc48b749b32dbd79f0c248b5cb16dfffb138c
appitoriadev <contact@appitoria.dev>
2024-07-06 09:21:14 -0500

    style: Update button styles and add category dropdown

--- js/tablalibros.js
+++ js/tablalibros.js
@@ -1,14 +1,8 @@
 
-let url = '/PDFs/'; // Ruta de la carpeta que contiene los archivos PDF.
-/*
- Agrega Event Listener a la página para poblar una tabla y una lista de materiales de audio/video. 
- La tabla se llena con datos del array sinCategoría, y la lista se llena con datos del array sinCategoría2. 
- Ambos arrays contienen información sobre materiales de audio y video, incluyendo URLs para escuchar, descargar y ver.
- */
+const url = '/PDFs/'; // Ruta de la carpeta que contiene los archivos PDF.
 
-window.addEventListener('load', () => {
-    
 const conCategoría = [
+    // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
     ['1', 'CON LOS PELOS DE PUNTA', 'Historia', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video'],
     ['2', 'ERASE UNA VEZ EN COLOMBIA', 'Juegos', 'URL-Spreaker', 'ERASE-UNA-VEZ-EN-COLOMBIA.pdf', 'URL-video'],
     ['3', 'TODO LO CONTRARIO', 'Adultos', 'URL-Spreaker', 'TODO-LO-CONTRARIO.pdf', 'URL-video'],
@@ -17,22 +11,130 @@ const conCategoría = [
     ['6', 'salud para contar', 'Adultos', 'URL-Spreaker', 'SALUD-PARA-CONTAR-2024.pdf', 'URL-video']
 ];
 
-localStorage.setItem('conCategoría', JSON.stringify(conCategoría));
+let historia = [];
+let juegos = [];
+let adultos = [];
+let niños = [];
+const listaCategoria = [
+    ['historia', historia],
+    ['juegos', juegos],
+    ['adultos', adultos],
+    ['niños', niños]
+];
+
+window.addEventListener('load', () => {
+
+    localStorage.setItem('conCategoría', JSON.stringify(conCategoría));
     //EJEMPLO CON DIVs
     let divPadre = document.getElementById('listalibros');
-    
-    conCategoría.forEach((fila, index) => {
-        
+
+    conCategoría.forEach((fila) => {
+
         let divHijoPorTítulo = document.createElement('div');
         divHijoPorTítulo.classList.add('elemento');
         let id = fila[0];
         fila.forEach((element, index) => {
-            
+
             a = document.createElement('a');
             h3 = document.createElement('h3');
-            btnLeer = document.createElement('button');
+            // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
             switch (index) {
-                case 0:                    
+                case 0:
+                    break;
+                case 1:
+                    h3.textContent = element;
+                    divHijoPorTítulo.appendChild(h3);
+                    break;
+                case 2:
+                    filtrarCategoria(fila, element);
+                    break;
+                case 3:
+                    a.href = element;
+                    a.target = '_blank';
+                    a.textContent = 'Escuchar';
+                    divHijoPorTítulo.appendChild(a);
+                    break;
+                case 4:
+                    a.href = url + element;
+                    a.download = url + element;
+                    a.textContent = 'Descargar';
+                    divHijoPorTítulo.appendChild(a);
+                    break;
+                case 5:
+                    a.href = element;
+                    a.target = '_blank';
+                    a.textContent = 'Videos';
+                    divHijoPorTítulo.appendChild(a);
+                    break;
+                default:
+                    let urlForm = '/leerLibro.html?id=' + id;
+                    a.href = urlForm;
+                    a.target = '_blank';
+                    a.textContent = 'Leer';
+                    a.classList.add('btnLeer');
+                    divHijoPorTítulo.appendChild(a);
+                    break;
+
+
+            }
+            divPadre.appendChild(divHijoPorTítulo);
+        });
+    });
+
+});
+
+/*-------------------------------------------------------------*/
+
+function filtrarCategoria(filaDeLibro, categoria) {
+
+    switch (categoria) {
+        case 'Historia':
+            if (!historia.includes(filaDeLibro))
+                historia.push(filaDeLibro);
+            break;
+        case 'Juegos':
+            if (!juegos.includes(filaDeLibro))
+                juegos.push(filaDeLibro);
+            break;
+        case 'Adultos':
+            if (!adultos.includes(filaDeLibro))
+                adultos.push(filaDeLibro);
+            break;
+        case 'Niños':
+            if (!niños.includes(filaDeLibro))
+                niños.push(filaDeLibro);
+            break;
+    }
+}
+
+/*-------------------------------------------------------------*/
+function leerLibro(idLibro) {
+
+    window.location = urlForm;
+}
+
+const btnAdultos = document.querySelector('#btnAdultos');
+btnAdultos.addEventListener('click', () => {
+    let listaOcultar = document.getElementById('listalibros');
+    listaOcultar.style.display = 'none';
+    let divPadre = document.getElementById('listaAdultos');
+
+    if(divPadre.childNodes.length >= 0){
+        divPadre.innerHTML = '';
+    }
+
+    adultos.forEach((fila) => {
+
+        let divHijoPorTítulo = document.createElement('div');
+        divHijoPorTítulo.classList.add('elemento');
+        let id = fila[0];
+        fila.forEach((element, index) => {
+
+            a = document.createElement('a');
+            h3 = document.createElement('h3');
+            // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
+            switch (index) {
+                case 0:
                     break;
                 case 1:
                     h3.textContent = element;
@@ -57,63 +159,82 @@ localStorage.setItem('conCategoría', JSON.stringify(conCategoría));
                     divHijoPorTítulo.appendChild(a);
                     break;
                 default:
-                    let urlForm = '/leerLibro.html?id='+id;
+                    let urlForm = '/leerLibro.html?id=' + id;
                     a.href = urlForm;
                     a.target = '_blank';
                     a.textContent = 'Leer';
                     a.classList.add('btnLeer');
                     divHijoPorTítulo.appendChild(a);
                     break;
-            
 
             }
             divPadre.appendChild(divHijoPorTítulo);
         });
     });
-
-    // filtrarCategoria();
-
 });
 
-/*-------------------------------------------------------------*/
+// const ddlCategorias = document.querySelector('#ddlCategorias');
+// ddlCategorias.addEventListener('change', () => {
 
-// function filtrarCategoria() {
+//     let categoria = ddlCategorias.value;
+//     let categoriaSeleccionada = listaCategoria.find((element) => element[0] == categoria);
+//     console.log('categoriaSeleccionada', categoriaSeleccionada);
+//     console.log('categoria', categoria);
+//     // console.log('listaCategoria', listaCategoria);
+//     let listaOcultar = document.getElementById('listalibros');
+//     if (listaOcultar.style.display !== 'none') {
+//         listaOcultar.style.display = 'none';
+//     }
+//     let divPadre = document.getElementById('listaAdultos');
 
-//     let historia = [];
-//     let juegos = [];
-//     let adultos = [];
-
-//     /*
-//     let conCategoría = [
-//         ['CON LOS PELOS DE PUNTA', 'Historia', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video'],
-//         ['CON LOS PELOS DE PUNTA', 'Juegos', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video'],
-//         ['CON LOS PELOS DE PUNTA', 'Adultos', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video']
-//     ];
-//     */
-//     conCategoría.forEach((fila, index) => {
-//         let categoria = '';
+//     if (divPadre.childNodes.length > 0) {
+//         divPadre.innerHTML = '';
+//     }
+//     categoriaSeleccionada.forEach((fila) => {
+//         let divHijoPorTítulo = document.createElement('div');
+//         divHijoPorTítulo.classList.add('elemento');
+//         let id = fila[0];
 //         fila.forEach((element, index) => {
-//             categoria = element[2];
-//         });
-//         switch (categoria) {
-//             case 'Historia':
-//                 if (!historia.includes(fila))
-//                     historia.push(fila);
-//                 break;
-//             case 'Juegos':
-//                 if (!juegos.includes(fila))
-//                     juegos.push(fila);
-//                 break;
-//             case 'Adultos':
-//                 if (!adultos.includes(fila))
-//                     adultos.push(fila);
-//                 break;
-//         }
-//     });
-// }
 
-/*-------------------------------------------------------------*/
-function leerLibro(idLibro){
-    
-    window.location = urlForm;
-}
+//             a = document.createElement('a');
+//             h3 = document.createElement('h3');
+//             // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
+//             switch (index) {
+//                 case 0:
+//                     break;
+//                 case 1:
+//                     h3.textContent = element;
+//                     divHijoPorTítulo.appendChild(h3);
+//                     break;
+//                 case 3:
+//                     a.href = element;
+//                     a.target = '_blank';
+//                     a.textContent = 'Escuchar';
+//                     divHijoPorTítulo.appendChild(a);
+//                     break;
+//                 case 4:
+//                     a.href = url + element;
+//                     a.download = url + element;
+//                     a.textContent = 'Descargar';
+//                     divHijoPorTítulo.appendChild(a);
+//                     break;
+//                 case 5:
+//                     a.href = element;
+//                     a.target = '_blank';
+//                     a.textContent = 'Videos';
+//                     divHijoPorTítulo.appendChild(a);
+//                     break;
+//                 default:
+//                     let urlForm = '/leerLibro.html?id=' + id;
+//                     a.href = urlForm;
+//                     a.target = '_blank';
+//                     a.textContent = 'Leer';
+//                     a.classList.add('btnLeer');
+//                     divHijoPorTítulo.appendChild(a);
+//                     break;
+//             }
+//             divPadre.appendChild(divHijoPorTítulo);
+//         });
+//     });
+
+// });
