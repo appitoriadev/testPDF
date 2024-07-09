52b1f03340a94b0c83145cf64a0fc9925680af43
appitoriadev <contact@appitoria.dev>
2024-07-09 10:29:38 -0500

    style: Update margin-top for body and div elements

--- js/leerLibro.js
+++ js/leerLibro.js
@@ -2,6 +2,7 @@
 
 const urlParams = new URLSearchParams(window.location.search);
 const bookId = urlParams.get('id');
+console.log('bookId',bookId);
 let pdfLibro = '/PDFs/';
 const conCategoría = JSON.parse(localStorage.getItem('conCategoría'));
 
@@ -16,6 +17,7 @@ function getBook(id){
         if(fila[0] == id){
             pdfLibro += fila[4];
             console.log('pdfLibro',pdfLibro);
+            return;
         }
     });
 }
@@ -61,43 +63,6 @@ function mostrarPDF(url){
     });
 }
 
-function mostrarPDF(url){
-
-    // Asignar a la variable pdfjsLib
-    const pdfjsLib = window['pdfjs-dist/build/pdf'];
-
-    // Asignar a la variable de trabajador
-    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
-
-    // Cargar el PDF
-    pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
-        const pdfViewer = document.getElementById('pdf-viewer');
-        let pdfDoc = pdfDoc_;
-        let pageNum = 1;
-
-        // Función para renderizar una página
-        function renderPage(num) {
-            pdfDoc.getPage(num).then(function(page) {
-                const viewport = page.getViewport({ scale: 1.5 });
-                const canvas = document.createElement('canvas');
-                const context = canvas.getContext('2d');
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Renderizar la página en el canvas
-                const renderContext = {
-                    canvasContext: context,
-                    viewport: viewport
-                };
-                page.render(renderContext);
-
-                pdfViewer.appendChild(canvas);
-            });
-        }
-
-        // Renderizar todas las páginas
-        for(let num = 1; num <= pdfDoc.numPages; num++) {
-            renderPage(num);
-        }
-    });
+function addBook(){
+    let libroNuevo =['1', 'CON LOS PELOS DE PUNTA', 'Historia', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video'];
 }
--- js/tablalibros.js
+++ js/tablalibros.js
@@ -1,8 +1,9 @@
 
 const url = '/PDFs/'; // Ruta de la carpeta que contiene los archivos PDF.
 
+window.addEventListener('load', () => {
+    
 const conCategoría = [
-    // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
     ['1', 'CON LOS PELOS DE PUNTA', 'Historia', 'URL-Spreaker', 'CON-LOS-PELOS-DE-PUNTA.pdf', 'URL-video'],
     ['2', 'ERASE UNA VEZ EN COLOMBIA', 'Juegos', 'URL-Spreaker', 'ERASE-UNA-VEZ-EN-COLOMBIA.pdf', 'URL-video'],
     ['3', 'TODO LO CONTRARIO', 'Adultos', 'URL-Spreaker', 'TODO-LO-CONTRARIO.pdf', 'URL-video'],
@@ -11,35 +12,22 @@ const conCategoría = [
     ['6', 'salud para contar', 'Adultos', 'URL-Spreaker', 'SALUD-PARA-CONTAR-2024.pdf', 'URL-video']
 ];
 
-let historia = [];
-let juegos = [];
-let adultos = [];
-let niños = [];
-const listaCategoria = [
-    ['historia', historia],
-    ['juegos', juegos],
-    ['adultos', adultos],
-    ['niños', niños]
-];
-
-window.addEventListener('load', () => {
-
-    localStorage.setItem('conCategoría', JSON.stringify(conCategoría));
+localStorage.setItem('conCategoría', JSON.stringify(conCategoría));
     //EJEMPLO CON DIVs
     let divPadre = document.getElementById('listalibros');
-
+    
     conCategoría.forEach((fila) => {
-
+        
         let divHijoPorTítulo = document.createElement('div');
         divHijoPorTítulo.classList.add('elemento');
         let id = fila[0];
         fila.forEach((element, index) => {
-
+            
             a = document.createElement('a');
             h3 = document.createElement('h3');
-            // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
+            btnLeer = document.createElement('button');
             switch (index) {
-                case 0:
+                case 0:                    
                     break;
                 case 1:
                     h3.textContent = element;
@@ -73,17 +61,18 @@ window.addEventListener('load', () => {
                     a.textContent = 'Leer';
                     a.classList.add('btnLeer');
                     divHijoPorTítulo.appendChild(a);
-                    break;
-
+                    break;            
 
             }
             divPadre.appendChild(divHijoPorTítulo);
         });
     });
 
+    // filtrarCategoria();
+
 });
 
-/*-------------------------------------------------------------*/
+/*----------------------------------------------------*/
 
 function filtrarCategoria(filaDeLibro, categoria) {
 
@@ -107,11 +96,8 @@ function filtrarCategoria(filaDeLibro, categoria) {
     }
 }
 
-/*-------------------------------------------------------------*/
-function leerLibro(idLibro) {
+/*----------------------------------------------------*/
 
-    window.location = urlForm;
-}
 
 const btnAdultos = document.querySelector('#btnAdultos');
 btnAdultos.addEventListener('click', () => {
@@ -173,68 +159,9 @@ btnAdultos.addEventListener('click', () => {
     });
 });
 
-// const ddlCategorias = document.querySelector('#ddlCategorias');
-// ddlCategorias.addEventListener('change', () => {
+/*----------------------------------------------------*/
 
-//     let categoria = ddlCategorias.value;
-//     let categoriaSeleccionada = listaCategoria.find((element) => element[0] == categoria);
-//     console.log('categoriaSeleccionada', categoriaSeleccionada);
-//     console.log('categoria', categoria);
-//     // console.log('listaCategoria', listaCategoria);
-//     let listaOcultar = document.getElementById('listalibros');
-//     if (listaOcultar.style.display !== 'none') {
-//         listaOcultar.style.display = 'none';
-//     }
-//     let divPadre = document.getElementById('listaAdultos');
-
-//     if (divPadre.childNodes.length > 0) {
-//         divPadre.innerHTML = '';
-//     }
-//     categoriaSeleccionada.forEach((fila) => {
-//         let divHijoPorTítulo = document.createElement('div');
-//         divHijoPorTítulo.classList.add('elemento');
-//         let id = fila[0];
-//         fila.forEach((element, index) => {
-
-//             a = document.createElement('a');
-//             h3 = document.createElement('h3');
-//             // ['ID', 'Título', 'Categoría', 'URL-Spreaker', 'PDF', 'URL-video']
-//             switch (index) {
-//                 case 0:
-//                     break;
-//                 case 1:
-//                     h3.textContent = element;
-//                     divHijoPorTítulo.appendChild(h3);
-//                     break;
-//                 case 3:
-//                     a.href = element;
-//                     a.target = '_blank';
-//                     a.textContent = 'Escuchar';
-//                     divHijoPorTítulo.appendChild(a);
-//                     break;
-//                 case 4:
-//                     a.href = url + element;
-//                     a.download = url + element;
-//                     a.textContent = 'Descargar';
-//                     divHijoPorTítulo.appendChild(a);
-//                     break;
-//                 case 5:
-//                     a.href = element;
-//                     a.target = '_blank';
-//                     a.textContent = 'Videos';
-//                     divHijoPorTítulo.appendChild(a);
-//                     break;
-//                 default:
-//                     let urlForm = '/leerLibro.html?id=' + id;
-//                     a.href = urlForm;
-//                     a.target = '_blank';
-//                     a.textContent = 'Leer';
-//                     a.classList.add('btnLeer');
-//                     divHijoPorTítulo.appendChild(a);
-//                     break;
-//             }
-//             divPadre.appendChild(divHijoPorTítulo);
-//         });
-//     });
-
-// });
+function leerLibro(idLibro){
+    
+    window.location = urlForm;
+}
